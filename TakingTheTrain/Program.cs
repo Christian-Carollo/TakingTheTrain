f35310671cc42aac6013e214955990e1fb44bf86
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-15 16:25:35 +0100

    Aggiungere i file di progetto.

---
+++ TakingTheTrain/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections.Generic;
+using TakingTheTrain;
+
+
+internal static class LinkedListExtensions
+{
+    public static void GetPercorso<Stazione>(this LinkedList<Stazione> percorsoFerroviario)
+    {
+        foreach (var elemento in percorsoFerroviario)
+        {
+            Console.WriteLine(elemento + "\n");
+        }
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        LinkedList<Stazione> percorsoFerroviario = new LinkedList<Stazione>();
+
+        percorsoFerroviario.AddLast(new Stazione("Palermo"));
+        percorsoFerroviario.AddLast(new Stazione("Catania"));
+        percorsoFerroviario.AddLast(new Stazione("Trapani"));
+
+        Console.WriteLine("Le stazioni presenti nell'iter ferroviario sono:\n");
+
+        percorsoFerroviario.GetPercorso();
+
+        percorsoFerroviario.RemoveFirst();
+        percorsoFerroviario.RemoveLast();
+
+
+
+        Console.WriteLine("Le stazioni presenti nell'iter ferroviario sono:\n");
+
+        percorsoFerroviario.GetPercorso();
+
+    }
+}
---
+++ TakingTheTrain/Stazione.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakingTheTrain
+{
+    internal class Stazione
+    {
+
+        public string? nome { get; set; }
+
+        public Stazione(string nome)
+        {
+            this.nome = nome;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Stazione di: {nome}";
+        }
+    }
+}
